ea11a718fbbdb2a599dfec2056a998565fd15652
hamed <hamed.hp332@gmail.com>
2022-03-28 02:52:36 +0430

    try improve

--- lib/page/home_view/home.dart
+++ lib/page/home_view/home.dart
@@ -12,25 +12,50 @@ class Home extends HookWidget {
         Jalali.now().copy(hour: 0, minute: 0, second: 0, millisecond: 0));
     final todayDate = useState(
         Jalali.now().copy(hour: 0, minute: 0, second: 0, millisecond: 0));
+
     final totalMonth = useState(21);
 
+    final minesDays = useState(0);
+    // extra days -is bad -the colors is red
+    final extraDays = useState(0);
+
+    final minesDaysController =
+        useTextEditingController(text: minesDays.value.toString());
     final totalMonthController =
         useTextEditingController(text: totalMonth.value.toString());
+    final extraDaysController =
+        useTextEditingController(text: extraDays.value.toString());
 
-    final finalDate = startDate.value.copy().addMonths(totalMonth.value);
-    final remainingDay = finalDate.distanceFrom(todayDate.value);
+    final lastDate = startDate.value.copy().addMonths(totalMonth.value);
+
+    final lastDateWithAll =
+        lastDate.addDays(-minesDays.value).addDays(extraDays.value);
+
+// start to end day with mines and extra days
+    final totalDays = startDate.value.distanceTo(lastDate);
+    final totalDaysWithAll = startDate.value.distanceTo(lastDateWithAll);
 
     final passedDay = todayDate.value.distanceFrom(startDate.value);
+    final passedDayPercent =
+        todayDate.value.distanceFrom(startDate.value) / totalDaysWithAll * 100;
 
-    final passedDayPercent = todayDate.value.distanceFrom(startDate.value) /
-        startDate.value
-            .copy()
-            .addMonths(totalMonth.value)
-            .distanceFrom(startDate.value) *
-        100;
-
+    final remainingDay = lastDateWithAll.distanceFrom(todayDate.value);
     final remaindedDayPercent = 100 - passedDayPercent;
     //${remainingDay / startDate.value.copy().addMonths(totalMonth.value).distanceFrom(startDate.value) * 100
+
+    // final minesDayPercent = minesDays.value /
+    //     startDate.value
+    //         .copy()
+    //         .addMonths(totalMonth.value)
+    //         .distanceFrom(startDate.value) *
+    //     100;
+
+    // final extraDayPercent = extraDays.value /
+    //     startDate.value
+    //         .copy()
+    //         .addMonths(totalMonth.value)
+    //         .distanceFrom(startDate.value) *
+    //     100;
 
     return ScaffoldPage.scrollable(
       header: const PageHeader(title: Text('Home')),
@@ -39,28 +64,100 @@ class Home extends HookWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Expanded(child: dateInput(context, 'Start', startDate)),
-            Expanded(child: Row(children: [])),
+            const SizedBox(width: 20),
+            Expanded(child: dateInput(context, "Today", todayDate)),
           ],
         ),
-        dateInput(context, "Today", todayDate),
 
-        const Text("Total Month"),
-        TextBox(
-          controller: totalMonthController,
-          onChanged: (value) {
-            totalMonth.value = int.parse(value);
-          },
+        Row(
+          children: [
+            Expanded(
+              child: Column(
+                children: [
+                  const Text("Total Month"),
+                  TextBox(
+                    controller: totalMonthController,
+                    onChanged: (value) {
+                      totalMonth.value = int.parse(value);
+                    },
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(width: 20),
+            Expanded(
+              child: Column(
+                children: [
+                  const Text("Mines days"),
+                  TextBox(
+                    controller: minesDaysController,
+                    onChanged: (value) {
+                      minesDays.value = int.parse(value);
+                    },
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(width: 20),
+            Expanded(
+              child: Column(
+                children: [
+                  const Text("Extra days"),
+                  TextBox(
+                    controller: extraDaysController,
+                    onChanged: (value) {
+                      extraDays.value = int.parse(value);
+                    },
+                  ),
+                ],
+              ),
+            )
+          ],
         ),
 
         // total days
         Text(
-            'Total days: ${startDate.value.copy().addMonths(totalMonth.value).distanceFrom(startDate.value)}'),
+            'Total days: $totalDays + ${extraDays.value} - ${minesDays.value} = $totalDaysWithAll'),
 
         // pass days and percent of days
         Text('Pass days: $passedDay ($passedDayPercent%)'),
 
         // remaining days and percent of days
         Text('Remain days: $remainingDay ($remaindedDayPercent%)'),
+
+        // prosess bar to show days
+        // Expanded(
+        //   child: Container(
+        //     decoration: BoxDecoration(
+        //       color: Colors.grey[40],
+        //       borderRadius: BorderRadius.circular(10),
+        //     ),
+        //     height: 10,
+        //     child: Row(
+        //       children: [
+        //         // total day
+        //         Expanded(
+        //           flex: totaldays - minesDays.value,
+        //           child: Container(
+        //               // height: 10,
+        //               // color: Colors.yellow,
+        //               ),
+        //         ),
+
+        //         Expanded(
+        //           flex: extraDays.value,
+        //           child: Container(
+        //             height: 10,
+        //             decoration: BoxDecoration(
+        //               color: Colors.red['lighter'],
+        //               borderRadius: BorderRadius.circular(10),
+        //             ),
+        //           ),
+        //         ),
+        //       ],
+        //     ),
+        //   ),
+        // ),
 
         // prosess bar to show days
         Expanded(
@@ -70,12 +167,11 @@ class Home extends HookWidget {
               borderRadius: BorderRadius.circular(10),
             ),
             height: 10,
-            width: 100,
             child: Row(
               children: [
                 // passed
                 Expanded(
-                  flex: (passedDayPercent * 100).toInt(),
+                  flex: passedDay,
                   child: Container(
                     decoration: BoxDecoration(
                       color: Colors.grey[90],
@@ -87,12 +183,85 @@ class Home extends HookWidget {
 
                 // remainded
                 Expanded(
-                  flex: (remaindedDayPercent * 100).toInt(),
+                  flex: remainingDay,
                   child: Container(
                       // height: 10,
                       // color: Colors.yellow,
                       ),
                 ),
+
+                // Expanded(
+                //   flex: (minesDayPercent * 100).toInt(),
+                //   child: Container(
+                //     height: 10,
+                //     decoration: BoxDecoration(
+                //       color: Colors.green['lighter'],
+                //       borderRadius: BorderRadius.circular(10),
+                //     ),
+                //   ),
+                // ),
+
+                // mines
+                // Expanded(
+                //   flex: minesDays.value,
+                //   child: Container(
+                //     height: 10,
+                //     decoration: BoxDecoration(
+                //       color: Colors.grey[40],
+                //       borderRadius: BorderRadius.circular(10),
+                //     ),
+                //   ),
+                // ),
+              ],
+            ),
+          ),
+        ),
+
+// prosess bar to show mines days
+        Expanded(
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.grey[40],
+              borderRadius: BorderRadius.circular(10),
+            ),
+            height: 10,
+            child: Row(
+              children: [
+                // total - mines
+                Expanded(
+                  flex: totalDays - minesDays.value,
+                  child: Container(
+                      // decoration: BoxDecoration(
+                      //   color: Colors.grey[90],
+                      //   borderRadius: BorderRadius.circular(10),
+                      // ),
+                      // height: 10,
+                      ),
+                ),
+
+                // mines
+                Expanded(
+                  flex: minesDays.value,
+                  child: Container(
+                    height: 10,
+                    decoration: BoxDecoration(
+                      color: Colors.green['lighter'],
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),
+
+                // extra
+                // Expanded(
+                // flex: extraDays.value,
+                // child: Container(
+                // height: 10,
+                // decoration: BoxDecoration(
+                //   color: Colors.green['lighter'],
+                //   borderRadius: BorderRadius.circular(10),
+                // ),
+                // ),
+                // ),
               ],
             ),
           ),
